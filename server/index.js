1222e1f4874ad6ad2787642c3eeb26f44b4e0e35
Ankur Phani <ankur@the-algo.com>
2022-01-08 12:48:26 +0530

    server push

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const daySale = require('./routes/daySale')
+const port = 5000;
+
+app.use(express.json())
+app.use('/daysale', daySale)
+
+app.get('/',(req, res)=>{
+    res.send('Hello hi there I am Gunu')
+    console.log('get method hit')
+})
+app.listen(port, (req, res) => {
+    console.log("backend running on ", port)
+})
---
+++ server/routes/daySale.js
@@ -0,0 +1,132 @@
+const express = require('express')
+const { google } = require("googleapis")
+
+const router = express.Router()
+
+console.log('Daysale hit')
+
+router.post('/daily', async(req,res)=>{
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    })
+
+    //create client instancce
+    const client = await auth.getClient();
+
+    //instance of google sheet api
+
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+
+    const spreadsheetId = "1fXK7l-FNlxNZuHIPIRkfZNCQeXmKIiWt11BJeeOUWyM"
+
+    // //get metadata
+    // const metaData = await googleSheets.spreadsheets.get({
+    //     auth,
+    //     spreadsheetId,
+    // })
+
+    // //Read rows FROM sheet
+
+    // const getRows = await googleSheets.spreadsheets.values.get({
+    //     auth,
+    //     spreadsheetId,
+    //     range: "Subs!A:B"
+    // })
+
+    //Write rows TO sheet
+    const { month,openDate, gasUseDate, gasPrice, electricity, Firoz, Sanjeeb, chickenPrice, chickenInKG, dailyExpense, dailySale } = req.body;
+
+    await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId,
+        range: `${month}!A:L`,
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [
+                [openDate, gasUseDate, gasPrice, electricity, Firoz, Sanjeeb, chickenPrice, chickenInKG, dailyExpense, dailySale]
+            ],
+
+        }
+    })
+    // await googleSheets.spreadsheets.values.batchUpdate({
+    //     auth,
+    //     spreadsheetId,
+    //     // range: `${month}!B:M`,
+    //     resource: {
+    //       valueInputOption: "USER_ENTERED",
+    //         data: [
+    //           {
+    //             range: 'test1!B:B',
+    //             values: [[openDate]]
+    //           },
+    //           {
+    //             range: 'test1!H:H',
+    //             values: [[chickenPrice]]
+    //           },
+    //           {
+    //             range: 'test1!J:J',
+    //             values: [[dailyExpense]]
+    //           },
+    //           {
+    //             range: 'test1!K:K',
+    //             values: [[dailySale]]
+    //           },
+    //             // [gasUseDate, gasPrice, electricity, Firoz, Sanjeeb, chickenPrice, chickenInKG, dailyExpense, dailySale]
+    //         ],
+
+    //     }
+    // })
+
+    res.send('Day Sale Saved')
+})
+
+router.post('/createMonth', async (req, res)=>{
+
+    const auth = new google.auth.GoogleAuth({
+         keyFile: "credentials.json",
+         scopes: "https://www.googleapis.com/auth/spreadsheets",
+    })
+
+    //create client instancce
+   const client = await auth.getClient();
+   const spreadsheetId = "1fXK7l-FNlxNZuHIPIRkfZNCQeXmKIiWt11BJeeOUWyM"
+   const {month} = req.body;
+
+//     //instance of google sheet api
+
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  await googleSheets.spreadsheets.batchUpdate({
+    auth,
+    spreadsheetId,
+    requestBody: {
+        requests: [{
+          addSheet: {
+            properties: {
+              title: month,
+            }
+          }
+        }]
+      }
+})
+res.send('New Sheet Created')
+})
+
+// router.post('/', async(req, res) => {
+//     const contacts = new Contact({
+//         name: req.body.name,
+//         tech: req.body.tech,
+//         sub: req.body.sub
+//     })
+//     try{
+//        const data = await contacts.save()
+//        res.json(data)
+//     }catch(err){
+//         res.send(err)
+//     }
+// })
+
+module.exports = router
